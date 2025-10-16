36c80d43af1bbca9e29c9ea509874410a6fae442
Peter Ross <peteross@outlook.dk>
2025-10-16 21:02:17 +0200

    Extra feild for collection table

--- models/Collection.js
+++ models/Collection.js
@@ -4,7 +4,8 @@ const CollectionSchema = new mongoose.Schema({
     itemId: {type: String, required: true },
     wear: {type: String, required: true },
     float: {type: Number, required: true },
-    userName: {type: String, required: true}
+    userName: {type: String, required: true},
+    estimatedPrice: {type: Number, required: true}
 },{collection: 'user'})
 
 module.exports = mongoose.model("Collection", CollectionSchema);
