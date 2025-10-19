861ccef0d8b56f98a5844cdacdb7127ae8462e41
Peter Ross <peteross@outlook.dk>
2025-10-19 20:20:04 +0200

    Delete request for collections

--- routes/collections.js
+++ routes/collections.js
@@ -46,4 +46,33 @@ router.get("/:id", async(request,response) => {
   }
 })
 
+router.delete("/:id", async(request,response) => {
+  
+
+  try {
+
+   
+    const {id} = request.params;
+
+
+    if (!ObjectId.isValid(id)) {
+      return response.status(400).json({error: "Invalid ID!"})
+    }
+    
+   const item = await Collection.findOne({_id: new ObjectId(id)});
+
+     if (!item) {
+      return response.status(404).json({ error: 'Item not found' });
+    }
+    
+    // Return the item
+    response.status(200).json(item);
+
+     }
+     catch (err) {
+    console.error('Error fetching item:', err);
+    response.status(500).json({ error: 'Internal server error' });
+  }
+})
+
 module.exports = router;
